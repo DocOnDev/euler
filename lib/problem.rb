e367ebb72b547c43729d6f042f8747ea8c9bcbc9
Michael (Doc) Norton <docondev@gmail.com>
2012-01-27 20:49:25 -0500

    Generate list of multiples

--- lib/problem.rb
+++ lib/problem.rb
@@ -1,6 +1,11 @@
 class GetMultiples
+    attr_accessor :multiple_list
+    def initialize
+        @multiple_list = []
+    end
     def list
-        [@multiple]
+        (1...@limit).each {|number| multiple_list << number if number % @multiple == 0}
+        multiple_list
     end
     def under limit
         @limit = limit
--- spec/probem_spec.rb
+++ spec/probem_spec.rb
@@ -1,8 +1,14 @@
 require 'problem'
 
 describe GetMultiples do
+    let(:multiples) { GetMultiples.new }
     it "returns 3 for mulitples of 3 under 5" do
-        multiples = GetMultiples.new
         multiples.of(3).under(5).list.should == [3]
+    end
+    it "returns [3,6] for multiples of 3 under 7" do
+        multiples.of(3).under(7).list.should == [3,6]
+    end
+    it "return [3,6,9] for multiple of 3 under 10" do
+        multiples.of(3).under(10).list.should == [3,6,9]
     end
 end
