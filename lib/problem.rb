09d0f65b1d23c1a1d2932a3adf99ea261a1279db
Michael (Doc) Norton <docondev@gmail.com>
2012-01-29 19:11:49 -0500

    Factors of two

--- lib/problem.rb
+++ lib/problem.rb
@@ -1,6 +1,15 @@
 class PrimeFactors
     def generate product
-        [] if product < 1
-        [] << product
+        primes = []
+        if product > 1
+            while product % 2 == 0
+                primes << 2
+                product /= 2
+            end
+            if product > 1
+                primes << product
+            end
+        end
+        primes
     end
 end
--- spec/probem_spec.rb
+++ spec/probem_spec.rb
@@ -3,14 +3,23 @@ require 'problem'
 describe PrimeFactors do
     let(:factors) { PrimeFactors.new }
     context "generate" do
-        it "returns an array if integers" do
+        it "returns an array of integers" do
             factors.generate(0).should be_a Array
+        end
+        it "is [] for a product of 0" do
+            factors.generate(0).should == []
         end
         it "is [2] for a product of 2" do
             factors.generate(2).should == [2]
         end
         it "is [3] for a product of 3" do
             factors.generate(3).should == [3]
+        end
+        it "is [2,2] for a product of 4" do
+            factors.generate(4).should == [2,2]
+        end
+        it "is [2,2,2] for a product of 8" do
+            factors.generate(8).should == [2,2,2]
         end
     end
 end
