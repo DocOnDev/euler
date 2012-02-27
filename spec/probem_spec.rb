30c77ecc9154c4161a0a72be2de87c6018e05abd
Michael (Doc) Norton <docondev@gmail.com>
2012-02-26 21:28:55 -0500

    Refactor away duplicate declarations

--- spec/probem_spec.rb
+++ spec/probem_spec.rb
@@ -1,21 +1,20 @@
 require 'problem'
 
 describe PalindromeDetector do
+    let(:detector) { PalindromeDetector.new }
+
     context 'single digit' do
         it 'considers any single digit to be a palindrome' do
-            detector = PalindromeDetector.new
             (0..9).each { |digit| detector.is_palindrome?(1).should == true }
         end
     end
 
     context 'double digits' do
         it 'considers 11 to be a palindrome' do
-            detector = PalindromeDetector.new
             detector.is_palindrome?(11).should == true
         end
 
         it 'considers 12 to not be a palindrome' do
-            detector = PalindromeDetector.new
             detector.is_palindrome?(12).should == false
         end
     end
