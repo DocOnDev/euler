7ca244bbe06c02f00f228ea5cc6e611e08468121
Michael (Doc) Norton <docondev@gmail.com>
2012-03-06 07:29:41 -0800

    Add two integer context

--- spec/probem_spec.rb
+++ spec/probem_spec.rb
@@ -2,15 +2,17 @@ require 'problem'
 
 describe SmallestProduct do
     let(:small_prod) { SmallestProduct.new }
-    it "is 1 for 1 and 1" do
-        small_prod.calc(1,1).should == 1
-    end
+    context "two integers" do
+        it "is 1 for 1 and 1" do
+            small_prod.calc(1,1).should == 1
+        end
 
-    it "is 2 for 1 and 2" do
-        small_prod.calc(1,2).should == 2
-    end
+        it "is 2 for 1 and 2" do
+            small_prod.calc(1,2).should == 2
+        end
 
-    it "is 4 for 2 and 2" do
-        small_prod.calc(2,2).should == 4
+        it "is 4 for 2 and 2" do
+            small_prod.calc(2,2).should == 4
+        end
     end
 end
