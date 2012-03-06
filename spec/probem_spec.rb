7bfcd2fde9c5359d680b164a668cb2dd6d238b63
Michael (Doc) Norton <docondev@gmail.com>
2012-03-06 07:26:39 -0800

    Dry tests

--- spec/probem_spec.rb
+++ spec/probem_spec.rb
@@ -1,13 +1,12 @@
 require 'problem'
 
 describe SmallestProduct do
+    let(:small_prod) { SmallestProduct.new }
     it "is 1 for 1 and 1" do
-        small_prod = SmallestProduct.new
         small_prod.calc(1,1).should == 1
     end
 
     it "is 2 for 1 and 2" do
-        small_prod = SmallestProduct.new
         small_prod.calc(1,2).should == 2
     end
 end
