4b290b668b0a75bce6cd32e67edacc4a725b9a01
Michael (Doc) Norton <docondev@gmail.com>
2013-06-03 19:50:24 -0500

    completes problem 7

--- lib/problem.rb
+++ lib/problem.rb
@@ -1,5 +1,22 @@
 class Primes
+
+  def initialize
+    @primes = [1,2]
+  end
+
   def [](num)
-    13
+    add_prime until @primes[num]
+    @primes[num]
+  end
+
+  def add_prime
+    last = @primes[-1]
+    until is_prime(++last) do last += 1 end
+    @primes << last
+  end
+
+  def is_prime(num)
+    @primes[1..-1].each { |prime| return false if (num % prime == 0) }
+    true
   end
 end
--- spec/probem_spec.rb
+++ spec/probem_spec.rb
@@ -1,8 +1,21 @@
 require 'problem'
 
 describe "Primes" do
+  let(:primes) {Primes.new}
+
+  it "should know the 5th prime" do
+    primes[5].should be 11
+  end
+
   it "should know the 6th prime" do
-    primes = Primes.new
     primes[6].should be 13
+  end
+
+  it "should know the 7th prime" do
+    primes[7].should be 17
+  end
+
+  it "shoujld know the 10001st prime" do
+    primes[10001].should be 104743
   end
 end
