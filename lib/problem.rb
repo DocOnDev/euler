1db52700026d51af100c1dc636afef0d90e854e7
Michael (Doc) Norton <docondev@gmail.com>
2013-06-03 20:30:28 -0500

    recognizes a valid triplet and returns the product

--- lib/problem.rb
+++ lib/problem.rb
@@ -3,5 +3,14 @@ class Triplet
     raise StandardError, "Set too large" if triplet.length > 3
     raise StandardError, "Set too small" if triplet.length < 3
     raise StandardError, "Set cannot contain duplicates" if triplet.uniq.length < 3
+    @triplet = triplet
+  end
+
+  def valid?
+    @triplet[0] * @triplet[0] + @triplet[1] * @triplet[1] == @triplet[2] * @triplet[2]
+  end
+
+  def product
+    @triplet.inject(:*)
   end
 end
--- spec/probem_spec.rb
+++ spec/probem_spec.rb
@@ -12,4 +12,19 @@ describe 'triplet' do
   it 'fails if any of the elements are equivalent' do
     expect { Triplet.new([1,1,2]) }.to raise_error(StandardError, 'Set cannot contain duplicates')
   end
+
+  it 'recognizes a valid pythagorean triplet' do
+    triplet = Triplet.new([3,4,5])
+    triplet.should be_valid
+  end
+
+  it 'recognizes an invalid pythagorean tiplet' do
+    triplet = Triplet.new([4,5,6])
+    triplet.should_not be_valid
+  end
+
+  it 'knows the product of the triplet' do
+    triplet = Triplet.new([3,4,5])
+    triplet.product.should == 3*4*5
+  end
 end
